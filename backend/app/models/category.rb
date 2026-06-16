438ca5989847f86a677885b3f44fb4aa5cc27e50
Siege Songsong <cjgsongsong@gmail.com>
2026-06-16 19:04:58 +0800

    refactor: Remove trailing whitespaces

--- backend/app/models/category.rb
+++ backend/app/models/category.rb
@@ -1,8 +1,8 @@
 class Category < ApplicationRecord
   has_many :expenses, dependent: :destroy
 
-  validates :name, uniqueness: { case_sensitive: false }, format: { 
-    with: /\A[A-Z][A-Za-z0-9 ]*\z/, 
+  validates :name, uniqueness: { case_sensitive: false }, format: {
+    with: /\A[A-Z][A-Za-z0-9 ]*\z/,
     message: "must start with an uppercase letter followed by zero to many alphanumeric characters and spaces"
   }
 end
